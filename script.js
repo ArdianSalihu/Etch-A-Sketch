21bac007cb0a7a50cc0c0016a8e2598490967a87
Ardian Salihu <ardian.sal@hotmail.com>
2023-03-12 21:43:25 +0100

    Add grid to div with javascript

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = "repeat(16, 1fr)";
+    board.style.gridTemplateRows = "repeat(16, 1fr)";
+});
