5598f1ea96d9001e33bee0a237b5ba8cfd0954d9
Ardian Salihu <ardian.sal@hotmail.com>
2023-03-21 11:10:12 +0100

    Add functionality to the buttons

--- script.js
+++ script.js
@@ -9,22 +9,43 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+let color = "black";
+
 function createBoard(size) {
     let board = document.querySelector(".board");
-
+  
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    let divNum = size * size
-    
+  
+    let divNum = size * size;
+  
     for (let i = 0; i < divNum; i++) {
-        let div = document.createElement("div");
-        div.style.backgroundColor = "yellow";
-        board.insertAdjacentElement("beforeend", div);
+      let div = document.createElement("div");
+  
+      // add event listener to div elements
+      div.addEventListener("mouseover", function () {
+        if (color === "random") {
+          // call colorRandom() function if color is set to "random"
+          this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+        } else {
+          this.style.backgroundColor = color;
+        }
+      });
+  
+      board.insertAdjacentElement("beforeend", div);
     }
+  }
+  
+
+
+function changeColor(choice) {
+    color = choice;
 }
 
-
+function resetBoard() {
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+}
 
 function  getSize () {
     let input = prompt("What size would you like ?: ");
